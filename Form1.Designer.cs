03e2240e38593222148a7fb815c0dffa40ad6598
seunghun <cozy0614@gmail.com>
2024-01-06 16:51:27 +0900

    vol2

---
+++ Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace TheDavinciCodeVol2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.파일ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.열기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.편집ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restorationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBoxIpl1 = new OpenCvSharp.UserInterface.PictureBoxIpl();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pictureBoxIpl2 = new OpenCvSharp.UserInterface.PictureBoxIpl();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.madeByToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일ToolStripMenuItem,
+            this.편집ToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(404, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 파일ToolStripMenuItem
+            // 
+            this.파일ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.열기ToolStripMenuItem});
+            this.파일ToolStripMenuItem.Name = "파일ToolStripMenuItem";
+            this.파일ToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.파일ToolStripMenuItem.Text = "File";
+            // 
+            // 열기ToolStripMenuItem
+            // 
+            this.열기ToolStripMenuItem.Name = "열기ToolStripMenuItem";
+            this.열기ToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
+            this.열기ToolStripMenuItem.Text = "Open Image";
+            this.열기ToolStripMenuItem.Click += new System.EventHandler(this.열기ToolStripMenuItem_Click);
+            // 
+            // 편집ToolStripMenuItem
+            // 
+            this.편집ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.restorationToolStripMenuItem});
+            this.편집ToolStripMenuItem.Name = "편집ToolStripMenuItem";
+            this.편집ToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.편집ToolStripMenuItem.Text = "Edit";
+            // 
+            // restorationToolStripMenuItem
+            // 
+            this.restorationToolStripMenuItem.Name = "restorationToolStripMenuItem";
+            this.restorationToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.restorationToolStripMenuItem.Text = "Restoration";
+            this.restorationToolStripMenuItem.Click += new System.EventHandler(this.restorationToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.DarkRed;
+            this.groupBox1.Controls.Add(this.pictureBoxIpl1);
+            this.groupBox1.Location = new System.Drawing.Point(11, 40);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.groupBox1.Size = new System.Drawing.Size(178, 190);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Original Image";
+            // 
+            // pictureBoxIpl1
+            // 
+            this.pictureBoxIpl1.Location = new System.Drawing.Point(5, 15);
+            this.pictureBoxIpl1.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.pictureBoxIpl1.Name = "pictureBoxIpl1";
+            this.pictureBoxIpl1.Size = new System.Drawing.Size(169, 167);
+            this.pictureBoxIpl1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxIpl1.TabIndex = 0;
+            this.pictureBoxIpl1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Black;
+            this.groupBox2.Controls.Add(this.pictureBoxIpl2);
+            this.groupBox2.Location = new System.Drawing.Point(215, 40);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.groupBox2.Size = new System.Drawing.Size(178, 190);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Result Image";
+            // 
+            // pictureBoxIpl2
+            // 
+            this.pictureBoxIpl2.Location = new System.Drawing.Point(4, 15);
+            this.pictureBoxIpl2.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.pictureBoxIpl2.Name = "pictureBoxIpl2";
+            this.pictureBoxIpl2.Size = new System.Drawing.Size(170, 167);
+            this.pictureBoxIpl2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxIpl2.TabIndex = 0;
+            this.pictureBoxIpl2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(249, 257);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 42);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Make Davinci Code";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.ImageKey = "(없음)";
+            this.button1.Location = new System.Drawing.Point(139, 257);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 42);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Loading Text File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.madeByToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // madeByToolStripMenuItem
+            // 
+            this.madeByToolStripMenuItem.Name = "madeByToolStripMenuItem";
+            this.madeByToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.madeByToolStripMenuItem.Text = "made by";
+            this.madeByToolStripMenuItem.Click += new System.EventHandler(this.madeByToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(5F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TheDavinciCodeVol2.Properties.Resources.DaVinciCode;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(404, 317);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Buxton Sketch", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Crimson;
+            this.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.Name = "Form1";
+            this.Text = "The Davinci Code";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 파일ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 열기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 편집ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restorationToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private OpenCvSharp.UserInterface.PictureBoxIpl pictureBoxIpl1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private OpenCvSharp.UserInterface.PictureBoxIpl pictureBoxIpl2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem madeByToolStripMenuItem;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,409 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+namespace TheDavinciCodeVol2
+{
+    public partial class Form1 : Form
+    {
+        IplImage m_InputImage = null;
+        IplImage m_OutputImage = null;
+        List<string> convertStringList = new List<string>();
+        List<string> RealConvertData = new List<string>();
+        List<string> RestorationStringList = new List<string>();
+        byte[] temp = new byte[20000];
+        string Binarycode = string.Empty;
+        static int count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 20000; i++)
+            {
+                temp[i] = 0;
+            }groupBox1.Visible = false;
+            groupBox2.Visible = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            
+        }
+
+        private void 열기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string imgPath;
+
+                OpenFileDialog dlg = new OpenFileDialog();
+                                
+                dlg.Filter = "Image Files(*.bmp,*jpg) | *.bmp; *.jpg";
+                dlg.ShowDialog();
+                imgPath = dlg.FileName;
+
+                m_InputImage = new IplImage(imgPath, LoadMode.AnyColor);
+                Bitmap bmp = new Bitmap(m_InputImage.ToBitmap());
+
+                pictureBoxIpl1.ImageIpl = m_InputImage;
+                groupBox1.Visible = true;
+            }
+            catch { }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string txtPath;
+
+                OpenFileDialog dlg = new OpenFileDialog();
+
+                dlg.DefaultExt = "txt.*";
+                dlg.Filter = "Text Files(.txt)|*.txt";
+                dlg.ShowDialog();
+                txtPath = dlg.FileName;
+
+                temp = System.IO.File.ReadAllBytes(dlg.FileName);
+
+                for (int i = 0; i < temp.Count(); i++)
+                {
+                    string convertstring = Convert.ToString(temp[i], 2);
+                    if (int.Parse(convertstring) < 10000000)
+                    {
+                        string t = "0" + convertstring;
+                        if (t.Count() < 8)
+                        {
+                            t = "0" + t;
+                        }
+                        if (t.Count() < 8)
+                        {
+                            t = "0" + t;
+                        }
+
+                        if (t.Count() < 8)
+                        {
+                            t = "0" + t;
+                        }
+
+                        if (t.Count() < 8)
+                        {
+                            t = "0" + t;
+                        }
+
+                        convertstring = t;
+                    }
+                    convertStringList.Add(convertstring);
+                    for (int j = 0; j < convertstring.Length; j++)
+                    {
+                        RealConvertData.Add(convertstring[j].ToString());
+                    }
+                }
+            }
+            catch { }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (m_InputImage == null)
+            {
+                MessageBox.Show("Please select the ImageFile want to encrypt");
+                return;
+            }
+            if(temp[0] == 0)
+            {
+                MessageBox.Show("Please select the TextFile want to encrypt");
+                return;
+            }
+            try
+            {
+                string r = string.Empty, g = string.Empty, b = string.Empty, t = string.Empty;
+                m_OutputImage = m_InputImage.Clone();
+                int mR = 0;
+                int mB = 0;
+                int mG = 0;
+                int index = -1;
+                if (m_InputImage != null)
+                {
+                    if (temp[0] != 0)
+                    {
+                        int n = temp.Count();
+                        count = n;
+
+                        for (int y = 0; y < m_InputImage.Height; y++)
+                        {
+                            for (int x = 0; x < m_InputImage.Width; x++)
+                            {
+                                if (RealConvertData.Count == index)
+                                {
+                                    break;
+                                }
+                                if (index < RealConvertData.Count - 1)
+                                {
+                                    ConvertImage(y, x, ref index);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                MessageBox.Show("Encryption is complete! save the image file.");
+
+                pictureBoxIpl2.ImageIpl = m_OutputImage;
+                groupBox2.Visible = true;
+
+                SaveFileDialog save = new SaveFileDialog();
+                //save.DefaultExt = "bmp.*";
+                save.Filter = "Image Files(*.bmp,*jpg) | *.bmp; *.jpg";
+                save.ShowDialog();
+                //Bitmap bmp = new Bitmap(m_InputImage.ToBitmap());
+                //BitmapConverter.ToIplImage(bmp, m_InputImage);
+
+
+                Cv.SaveImage(save.FileName, m_InputImage);
+
+                //bmp.Save(save.FileName);
+            }
+            catch { }
+
+        }
+
+        private void ConvertImage(int y, int x,ref int _index)
+        {
+            try
+            {
+                int mR = 0;
+                int mB = 0;
+                int mG = 0;
+                CvColor c = m_InputImage[y, x];
+                mR = c.R % 2;
+                mG = c.G % 2;
+                mB = c.B % 2;
+                for (int i = 0; i < 3; i++)
+                {
+                    _index++;
+                    if (i == 0)
+                    {
+                        if (mR == 0)
+                        {
+                            if (RealConvertData[_index] == "1")
+                            {
+                                m_InputImage[y, x] = new CvColor
+                                {
+                                    B = c.B,
+                                    G = c.G,
+                                    R = (byte)(c.R + 1)
+                                };
+                                CvColor tempc = m_InputImage[y, x];
+                                c.R = tempc.R;
+                            }
+                        }
+                        if (mR == 1)
+                        {
+                            if (RealConvertData[_index] == "0")
+                            {
+                                m_InputImage[y, x] = new CvColor
+                                {
+                                    B = c.B,
+                                    G = c.G,
+                                    R = (byte)(c.R + 1)
+                                };
+                                CvColor tempc = m_InputImage[y, x];
+                                c.R = tempc.R;
+                            }
+                        }
+                    }
+                    if (i == 1)
+                    {
+                        if (mG == 0)
+                        {
+                            if (RealConvertData[_index] == "1")
+                            {
+                                m_InputImage[y, x] = new CvColor
+                                {
+                                    B = c.B,
+                                    G = (byte)(c.G + 1),
+                                    R = c.R
+                                };
+                                CvColor tempc = m_InputImage[y, x];
+                                c.G = tempc.G;
+                            }
+                        }
+                        if (mG == 1)
+                        {
+                            if (RealConvertData[_index] == "0")
+                            {
+                                m_InputImage[y, x] = new CvColor
+                                {
+                                    B = c.B,
+                                    G = (byte)(c.G + 1),
+                                    R = c.R
+                                };
+                                CvColor tempc = m_InputImage[y, x];
+                                c.G = tempc.G;
+                            }
+                        }
+                    }
+                    if (i == 2)
+                    {
+                        if (mB == 0)
+                        {
+                            if (RealConvertData[_index] == "1")
+                            {
+                                m_InputImage[y, x] = new CvColor
+                                {
+                                    B = (byte)(c.B + 1),
+                                    G = c.G,
+                                    R = c.R
+                                };
+                                CvColor tempc = m_InputImage[y, x];
+                                c.B = tempc.B;
+                            }
+                        }
+                        if (mB == 1)
+                        {
+                            if (RealConvertData[_index] == "0")
+                            {
+                                m_InputImage[y, x] = new CvColor
+                                {
+                                    B = (byte)(c.B + 1),
+                                    G = c.G,
+                                    R = c.R
+                                };
+                                CvColor tempc = m_InputImage[y, x];
+                                c.B = tempc.B;
+                            }
+                        }
+                    }
+                }
+            }
+            catch { }
+        }
+
+        private void restorationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Please select the Image want to Restore");
+            try
+            {
+                string imgPath;
+
+                OpenFileDialog dlg = new OpenFileDialog();
+
+                //dlg.DefaultExt = "bmp.*";
+                dlg.Filter = "Image Files(*.bmp,*jpg) | *.bmp; *.jpg";
+                dlg.ShowDialog();
+                imgPath = dlg.FileName;
+
+                m_OutputImage = new IplImage(imgPath, LoadMode.AnyColor);
+                //Bitmap bmp = new Bitmap(m_OutputImage.ToBitmap());            
+
+                pictureBoxIpl2.ImageIpl = m_OutputImage;
+                groupBox2.Visible = true;
+                Restoration();
+            }
+            catch {                
+            }
+        }
+
+        private void Restoration()
+        {
+            try
+            {
+                int mR = 0;
+                int mB = 0;
+                int mG = 0;
+
+                if (m_OutputImage != null)
+                {
+                    for (int y = 0; y < m_OutputImage.Height; y++)
+                    {
+                        for (int x = 0; x < m_OutputImage.Width; x++)
+                        {
+                            CvColor cOutput = m_OutputImage[y, x];
+                            mR = cOutput.R % 2;
+                            mG = cOutput.G % 2;
+                            mB = cOutput.B % 2;
+                            for (int i = 0; i < 3; i++)
+                            {
+                                if (i == 0)
+                                {
+                                    if (mR == 0)
+                                    {
+                                        RestorationStringList.Add("0");
+                                    }
+                                    if (mR == 1)
+                                    {
+                                        RestorationStringList.Add("1");
+                                    }
+                                }
+                                if (i == 1)
+                                {
+                                    if (mG == 0)
+                                    {
+                                        RestorationStringList.Add("0");
+                                    }
+                                    if (mG == 1)
+                                    {
+                                        RestorationStringList.Add("1");
+                                    }
+                                }
+                                if (i == 2)
+                                {
+                                    if (mB == 0)
+                                    {
+                                        RestorationStringList.Add("0");
+                                    }
+                                    if (mB == 1)
+                                    {
+                                        RestorationStringList.Add("1");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                byte[] resultbyte = new byte[1000000];
+                string temp = string.Empty;
+                for (int i = 0; i < RestorationStringList.Count(); i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        if (i * 8 + 8 < RestorationStringList.Count())
+                        {
+                            temp += RestorationStringList[i * 8 + j];
+                        }
+                    }
+                    if (temp != "" && i < resultbyte.Count() - 1)
+                    {
+                        resultbyte[i] = byte.Parse(Convert.ToInt16(temp, 2).ToString());
+                        temp = string.Empty;
+                    }
+                    if (i > resultbyte.Count())
+                    {
+                        break;
+                    }
+                }
+
+                MessageBox.Show("Translate success!! Save the file.");
+
+                string txtPath;
+                SaveFileDialog slg = new SaveFileDialog();
+                slg.DefaultExt = "txt.*";
+                slg.Filter = "Text Files(.txt)|*.txt";
+                slg.ShowDialog();
+                txtPath = slg.FileName;
+
+                System.IO.File.WriteAllBytes(txtPath, resultbyte);
+            }
+            catch { }
+        }
+
+        private void madeByToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("blog.naver.com/smhotkdg");
+        }       
+    }
+}
